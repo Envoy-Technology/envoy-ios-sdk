a1ce4fd7bfd49960cb8a0b85935d17baf7ae0f4a
k.kempa <>
2026-04-26 18:47:21 +0200

    Manage links endpoints

--- Sources/EnvoySDK/Networking/CreateLink/CreateLinkRequest.swift
+++ Sources/EnvoySDK/Networking/CreateLink/CreateLinkRequest.swift
@@ -9,11 +9,13 @@ public struct CreateLinkRequest: Codable {
     let sharerId: String
     let isSandbox: Bool?
     let labels: [Label]?
+    let isCarousel: Bool?
+    let isCarouselLink: Bool?
 
     public init(autoplay: Bool?, contentSetting: ContentSetting,
                 lifespanAfterClick: LifespanAfterClick? = nil,
                 openQuota: Int? = nil, extra: String? = nil,
-                sharerId: String, isSandbox: Bool? = nil, labels: [Label]? = nil) {
+                sharerId: String, isSandbox: Bool? = nil, labels: [Label]? = nil, isCarousel: Bool? = false, isCarouselLink: Bool? = false) {
         self.autoplay = autoplay
         self.contentSetting = contentSetting
         self.lifespanAfterClick = lifespanAfterClick
@@ -22,6 +24,8 @@ public struct CreateLinkRequest: Codable {
         self.sharerId = sharerId
         self.isSandbox = isSandbox
         self.labels = labels
+        self.isCarousel = isCarousel
+        self.isCarouselLink = isCarouselLink
     }
     
     // Define the Common structure
--- Sources/EnvoySDK/Networking/Endpoints.swift
+++ Sources/EnvoySDK/Networking/Endpoints.swift
@@ -3,18 +3,24 @@ import Foundation
 enum Endpoint {
     case createLink
     case prepLink
+    case manageLinks
+    case clearManagedLinks
     case getUerRemainingQuota(userId: String)
     case logPixelEvent
     case getUserRewards(userId: String)
     case claimUserReward
     case getUserCurrentRewards(userId: String)
-    
+
     var path: String {
         switch self {
         case .prepLink:
             return "prep-link"
         case .createLink:
             return "create-link"
+        case .manageLinks:
+            return "manage-links"
+        case .clearManagedLinks:
+            return "manage-links/clear"
         case .getUerRemainingQuota(let userId):
             return "user-quota/\(userId)"
         case .logPixelEvent:
